5772e3b1384653346cbed14a9e8d85ac1e2f6746
Sadiku Artan <artan.sadiku@he-arc.ch>
2023-02-11 12:27:38 +0100

    Initial commit with project files.

---
+++ FTApp/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FTApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly VideoCapture? videoCapture;
+        private Mat frame;
+        private bool startedCapture = false;
+
+        public MainWindow()
+        {
+            frame = new Mat();
+            InitializeComponent();
+            videoCapture = new VideoCapture(0); // 0 = first device detected (in the case of a laptop, it is the integrated webcam)
+
+            videoCapture.ImageGrabbed += VideoCapture_ImageGrabbed;
+        }
+
+        private void VideoCapture_ImageGrabbed(object? sender, EventArgs e)
+        {
+            videoCapture?.Retrieve(frame);
+
+            Dispatcher.BeginInvoke(new ThreadStart(delegate {
+                VideoImage.Source = ConvertBitmap(frame.ToBitmap());
+            }));
+        }
+
+        /// <summary>
+        /// Converts a Bitmap system image to a BitmapImage class.
+        /// </summary>
+        /// <param name="bitmap">The bitmap image</param>
+        /// <returns></returns>
+        private static BitmapImage ConvertBitmap(System.Drawing.Bitmap bitmap)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = memoryStream;
+            image.EndInit();
+
+            return image;
+        }
+
+        private void VideoCaptureButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!startedCapture)
+            {
+                videoCapture?.Start();
+                VideoCaptureButton.Content = "Stop video";
+            }
+            else
+            {
+                videoCapture?.Stop();
+                VideoCaptureButton.Content = "Start video";
+            }
+            startedCapture = !startedCapture;
+        }
+    }
+}
